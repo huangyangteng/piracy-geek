8b9766269785c8ba86a8d108a6179fda39e2d225
yellowyellow <yellowyellow12138@gmail.com>
2018-05-20 11:46:04 +0800

    个人主页-简介完成

---
+++ pages/user-home-page/user-home-page.js
@@ -0,0 +1,48 @@
+Page({
+    data:{
+        // `点击切换 作品 动态 简介 work dynamic intro
+        currentOption:'intro'
+    },
+    // `点击切换option
+    changeOption(e){
+        this.setData({
+            currentOption:e.target.dataset.option
+        })
+    },
+    onLoad:function(options){
+        // 生命周期函数--监听页面加载
+        
+    },
+    onReady:function(){
+        // 生命周期函数--监听页面初次渲染完成
+        
+    },
+    onShow:function(){
+        // 生命周期函数--监听页面显示
+        
+    },
+    onHide:function(){
+        // 生命周期函数--监听页面隐藏
+        
+    },
+    onUnload:function(){
+        // 生命周期函数--监听页面卸载
+        
+    },
+    onPullDownRefresh: function() {
+        // 页面相关事件处理函数--监听用户下拉动作
+        
+    },
+    onReachBottom: function() {
+        // 页面上拉触底事件的处理函数
+        
+    },
+    onShareAppMessage: function() {
+        // 用户点击右上角分享
+        return {
+          title: 'title', // 分享标题
+          desc: 'desc', // 分享描述
+          path: 'path' // 分享路径
+        }
+    }
+})
