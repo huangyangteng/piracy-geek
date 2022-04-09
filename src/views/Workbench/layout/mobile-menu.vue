4a664f769788c17b467f52a685567343cca27f80
huangyangteng <huangyangteng@ebupt.com>
2022-04-09 23:44:03 +0800

    feat:watch页面的某些功能增加权限校验

---
+++ src/views/Workbench/layout/mobile-menu.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <section></section>
+        <aside>展开</aside>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'mobile-menu',
+    props: {
+        units: {
+            type: Array,
+            default: () => []
+        },
+        active: {
+            type: [String, Number],
+            default: ''
+        }
+    },
+    data() {
+        return {}
+    },
+    computed: {},
+    methods: {},
+    created() {}
+}
+</script>
+
+<style lang="" scoped></style>
